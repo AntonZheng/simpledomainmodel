2bb9c1aeac2c504c494351309ed9e4a559f602c0
antonzheng <anton.zheng@gmail.com>
2018-02-02 21:47:22 -0800

    passed all tests

--- SimpleDomainModel/SimpleDomainModel/main.swift
+++ SimpleDomainModel/SimpleDomainModel/main.swift
@@ -26,13 +26,51 @@ open class TestMe {
 public struct Money {
   public var amount : Int
   public var currency : String
+    
+    init(amount: Int, currency: String){
+        self.amount = amount
+        self.currency = currency
+    }
+  
   
   public func convert(_ to: String) -> Money {
+    switch (self.currency, to){
+        case ("USD","GBP"):
+            return Money(amount: self.amount/2 , currency: to)
+        case ("USD","EUR"):
+            return Money(amount: (self.amount * 3)/2, currency: to)
+        case ("USD","CAN"):
+            return Money(amount: (self.amount * 5)/4, currency: to)
+        case ("GBP","USD"):
+            return Money(amount: self.amount*2, currency: to)
+        case ("GBP","EUR"):
+            return Money(amount: (self.amount * 9)/8, currency: to)
+        case ("GBP","CAN"):
+            return Money(amount: (self.amount * 19)/11, currency: to)
+        case ("EUR","USD"):
+            return Money(amount: (self.amount * 2)/3, currency: to)
+        case ("EUR","GBP"):
+            return Money(amount: (self.amount * 8)/9, currency: to)
+        case ("EUR","CAN"):
+            return Money(amount: (self.amount * 3)/2, currency: to)
+        case ("CAN","USD"):
+            return Money(amount: (self.amount * 4)/5, currency: to)
+        case ("CAN","GBP"):
+            return Money(amount: (self.amount * 11)/19, currency: to)
+        case ("CAN","EUR"):
+            return Money(amount: (self.amount * 2)/3, currency: to)
+        default:
+            return self
+    }
   }
   
   public func add(_ to: Money) -> Money {
+    let convert = self.convert(to.currency)
+    return Money(amount: convert.amount + to.amount, currency: to.currency)
   }
   public func subtract(_ from: Money) -> Money {
+    let convert = self.convert(from.currency)
+    return Money(amount: convert.amount - from.amount, currency: from.currency)
   }
 }
 
@@ -49,12 +87,26 @@ open class Job {
   }
   
   public init(title : String, type : JobType) {
+    self.title = title
+    self.type = type
   }
   
   open func calculateIncome(_ hours: Int) -> Int {
+    switch self.type {
+    case .Hourly(let hourly):
+        return Int(Double(hours) * hourly)
+    case .Salary(let salary):
+        return salary
+    }
   }
   
   open func raise(_ amt : Double) {
+    switch self.type {
+    case .Hourly(let hourly):
+        self.type = JobType.Hourly(hourly+amt)
+    case .Salary(let salary):
+        self.type = JobType.Salary(Int(Double(salary) + amt))
+    }
   }
 }
 
@@ -68,15 +120,23 @@ open class Person {
 
   fileprivate var _job : Job? = nil
   open var job : Job? {
-    get { }
+    get {
+        return self._job
+    }
     set(value) {
+        if(self.age >= 16){
+            self._job = value
+        }
     }
   }
   
   fileprivate var _spouse : Person? = nil
   open var spouse : Person? {
-    get { }
+    get {return self._spouse }
     set(value) {
+        if(self.age >= 18){
+            self._spouse = value
+        }
     }
   }
   
@@ -87,6 +147,7 @@ open class Person {
   }
   
   open func toString() -> String {
+    return "[Person: firstName:\(self.firstName) lastName:\(self.lastName) age:\(self.age) job:\(String(describing: self.job)) spouse:\(String(describing: self.spouse))]"
   }
 }
 
@@ -97,12 +158,23 @@ open class Family {
   fileprivate var members : [Person] = []
   
   public init(spouse1: Person, spouse2: Person) {
+    self.members.append(spouse1)
+    self.members.append(spouse2)
   }
   
   open func haveChild(_ child: Person) -> Bool {
+    members.append(child)
+    return true
   }
   
   open func householdIncome() -> Int {
+    var result = 0
+    for member in self.members {
+        if((member.job?.calculateIncome(2000)) != nil){
+            result += (member.job?.calculateIncome(2000))!
+        }
+    }
+    return result
   }
 }
 
